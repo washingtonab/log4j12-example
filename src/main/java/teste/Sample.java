e19fc718f02eb6b442495a45e12ba0b65a0e85c7
washingtonab <washington.ab@gmail.com>
2020-01-08 21:35:21 -0300

    first commit

--- src/main/java/teste/Sample.java
+++ src/main/java/teste/Sample.java
@@ -4,15 +4,37 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.MDC;
 
 public class Sample {
-	
+
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		System.out.println("Hello world");
-		
-		MDC.put("separator", "blablabla");
-		
+
+		MDC.put("separator", "000");
 		Logger logger = Logger.getLogger(Sample.class);
-		logger.info("hey");
+		logger.info(Thread.currentThread().getName());
+
+		
+		new Thread(
+				new Runnable() {
+					public void run() {
+						MDC.put("separator", "123");
+						Logger logger = Logger.getLogger(Sample.class);
+						logger.info(Thread.currentThread().getName());
+					}
+				}
+		).start();
+		
+
+		new Thread(
+				new Runnable() {
+					public void run() {
+						MDC.put("separator", "456");
+						Logger logger = Logger.getLogger(Sample.class);
+						logger.info(Thread.currentThread().getName());
+					}
+				}
+		).start();
+
 		
 		long end = System.currentTimeMillis();
 		System.out.println("Tempo de processamento em milisegundos: " + (end - start));
--- src/main/java/teste/SeparatorFileAppender.java
+++ src/main/java/teste/SeparatorFileAppender.java
@@ -1,6 +1,5 @@
 package teste;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.apache.log4j.FileAppender;
@@ -11,62 +10,43 @@ import org.apache.log4j.spi.LoggingEvent;
 
 public class SeparatorFileAppender extends FileAppender {
 
-	private final static String DOT = ".";
-	private final static String HIPHEN = "-";
-	private static final String ORIG_LOG_FILE_NAME = "OrginalLogFileName";
+	private static final String ORIGINAL_FILE_NAME = "orginalLogFileName";
+	private static final String SEPARATOR = "separator";
 
 	public SeparatorFileAppender() {
 	}
 
-	public SeparatorFileAppender(Layout layout, String fileName,
-			boolean append, boolean bufferedIO, int bufferSize)
-			throws IOException {
+	public SeparatorFileAppender(Layout layout, String fileName, boolean append, boolean bufferedIO, int bufferSize) throws IOException {
 		super(layout, fileName, append, bufferedIO, bufferSize);
 	}
 
-	public SeparatorFileAppender(Layout layout, String fileName,
-			boolean append) throws IOException {
+	public SeparatorFileAppender(Layout layout, String fileName, boolean append) throws IOException {
 		super(layout, fileName, append);
 	}
 
-	public SeparatorFileAppender(Layout layout, String fileName)
-			throws IOException {
+	public SeparatorFileAppender(Layout layout, String fileName) throws IOException {
 		super(layout, fileName);
 	}
 
 	@Override
 	public void activateOptions() {
-		MDC.put(ORIG_LOG_FILE_NAME, fileName);
+		MDC.put(ORIGINAL_FILE_NAME, fileName);
 		super.activateOptions();
 	}
 
 	@Override
 	public void append(LoggingEvent event) {
 		try {
-			//String fileName = appendLevelToFileName((String) MDC.get(ORIG_LOG_FILE_NAME), event.getLevel().toString());
-			String fileName = appendLevelToFileName((String) MDC.get(ORIG_LOG_FILE_NAME), (String) MDC.get("separator"));
-			setFile(fileName, fileAppend, bufferedIO, bufferSize);
-		} catch (IOException ie) {
-			errorHandler.error("Error occured while setting file for the log level " + event.getLevel(), ie, ErrorCode.FILE_OPEN_FAILURE);
+			String separator = (String) MDC.get(SEPARATOR);
+			String originalFileName = (String) MDC.get(ORIGINAL_FILE_NAME);
+			System.out.println(originalFileName);
+			String newFileName = originalFileName.concat("."+separator);
+			setFile(newFileName, fileAppend, bufferedIO, bufferSize);
+		} catch (Exception e) {
+			String message = "Error on separate log file";
+			errorHandler.error(message, e, ErrorCode.FILE_OPEN_FAILURE);
 		}
 		super.append(event);
 	}
 
-	private String appendLevelToFileName(String oldLogFileName, String separator) {
-		if (oldLogFileName != null) {
-			final File logFile = new File(oldLogFileName);
-			String newFileName = "";
-			final String fn = logFile.getName();
-			final int dotIndex = fn.indexOf(DOT);
-			if (dotIndex != -1) {
-				// the file name has an extension. so, insert the separator between the file name and the extension
-				newFileName = fn.substring(0, dotIndex) + HIPHEN + separator + fn.substring(dotIndex);
-			} else {
-				// the file name has no extension. So, just append the separator at the end
-				newFileName = fn+HIPHEN+separator;
-			}
-			return /* logFile.getParent()+File.separator+ */newFileName;
-		}
-		return null;
-	}
 }
